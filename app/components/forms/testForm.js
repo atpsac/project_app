486d2aff913c95f8a1660df09b8da9bf38e0d831
Cristian Angulo <paul.angulo.anyosa@gmail.com>
2019-08-22 17:29:34 -0500

    primer formulario

---
+++ app/components/forms/testForm.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import t from "tcomb-form-native";
+const Form = t.form.Form;
+
+export const LoginStruct = t.struct({
+    user: t.String,
+    password: t.String
+});
+
+export const LoginOptions = {
+    fields: {
+        user: {
+            label: "Nombre de usuario (*)",
+            placeholder: "Nombre de usuario"
+        },
+        password: {
+            label: "Constraseña (*)",
+            placeholder: "Contraseña",
+            password: true,
+            secureTextEntry: true
+        }
+    }
+}
